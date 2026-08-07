695b6c10e34d0d639eb57a935df518ed0668497b
cooler2024 <cooler09@foxmail.com>
2026-08-07 12:04:37 +0800

    Initial Commit

---
+++ PointWord/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PointWord
+//
+//  Created by ByteDance on 8/7/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PointWordTests/PointWordTests.swift
@@ -0,0 +1,16 @@
+//
+//  PointWordTests.swift
+//  PointWordTests
+//
+//  Created by ByteDance on 8/7/26.
+//
+
+import Testing
+
+struct PointWordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
